28a1dd468249527799f8126201955b6e4701cd9a
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-06-29 10:32:21 +0100

    update todo_list view

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,13 @@
+"""
+This module describes the todo model classes.
+"""
+
 from django.db import models
 
 class Item(models.Model):
+    """
+    Creating the Item model
+    """
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
 
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,16 @@
+"""
+This module defines the todo views.
+"""
+
 from django.shortcuts import render
+from .models import Item
 
 
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    """Render Todo List"""
+    items = Item.objects.all()
+
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
