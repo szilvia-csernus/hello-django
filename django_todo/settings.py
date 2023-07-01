a5ffcb150fe25d0626b7003f9fc0bc329b6ca65a
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-07-01 17:32:17 +0100

    update ALLOWED HOSTS

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = "DEVELOPMENT" in os.environ
 
 if development:
-    ALLOWED_HOSTS = ['127.0.0.1']
+    ALLOWED_HOSTS = [os.environ.get('LOCALHOST')]
 else:
     ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
