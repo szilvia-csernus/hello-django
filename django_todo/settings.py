540fa5d0d8593e14f1fd60175e7b25a17a2d170a
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-07-01 18:03:53 +0100

    another attempt to fix heroku bug

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 from dotenv import load_dotenv 
 load_dotenv()
 
-development = os.environ.get('DEVELOPMENT', False)
+development = os.environ.get('DEVELOPMENT')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
